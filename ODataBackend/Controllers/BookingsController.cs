f849ce2e08c1321d6ff2827f2986fbde5359b580
Nana Appiah <appiah@flexberry.net>
2024-07-26 21:54:36 +0500

    Implement GetAllRooms and GetSumOfRevenue methods

---
+++ ODataBackend/Controllers/BookingsController.cs
@@ -0,0 +1,49 @@
+﻿using ICSSoft.STORMNET;
+using ICSSoft.STORMNET.Business;
+using ICSSoft.STORMNET.Business.LINQProvider;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HAPI.Controllers
+{
+    [Route("api/Bookings")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly IDataService dataService;
+
+        public BookingsController(IDataService _dataService)
+        {
+            dataService = _dataService;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public ActionResult<Room> GetAllRooms()
+        {
+            var rooms = dataService.Query<Room>(Room.Views.RoomE).ToList();
+            return Ok(rooms);
+
+        }
+
+        [HttpGet]
+        [Route("TotalRevenue")]
+        public ActionResult<decimal> GetSumOfRevenue() 
+        {
+            var bookings = dataService.Query<Booking>(Booking.Views.BookingL).ToList();
+            decimal sumOfRevenue = 0;
+
+            foreach (var booking in bookings)
+            {
+                var daysBooked = (booking.CheckOut - booking.CheckIn).TotalDays;
+                var roomPrice = booking.Room.Price;
+
+                sumOfRevenue += (decimal)(daysBooked * roomPrice);
+            }
+            return Ok(sumOfRevenue);
+        }
+    }
+}
