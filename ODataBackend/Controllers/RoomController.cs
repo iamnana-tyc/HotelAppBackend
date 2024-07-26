db24610af16a828bbb7af6c71a5f07769b02d050
Nana Appiah <appiah@flexberry.net>
2024-07-26 21:52:30 +0500

    Implement isRoom free method

---
+++ ODataBackend/Controllers/RoomController.cs
@@ -0,0 +1,34 @@
+﻿using ICSSoft.STORMNET.Business;
+using ICSSoft.STORMNET.Business.LINQProvider;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using ICSSoft.STORMNET.KeyGen;
+
+namespace HAPI.Controllers
+{
+    [Route("api/Room")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IDataService dataService;
+
+        public RoomController(IDataService _dataService)
+        {
+            dataService = _dataService;
+        }
+
+        [HttpGet]
+        [Route("isRoomFree")]
+        public ActionResult IsRoomFree([FromQuery] Guid roomId, [FromQuery] DateTime dateToFilter)
+        {
+            Room room = new Room();
+            room.SetExistObjectPrimaryKey(roomId);
+
+            var bookings = dataService.Query<Booking>(Booking.Views.BookingE).Where(b => b.Room == room);
+            bool isFree = !bookings.Any(b => b.CheckIn.Date == dateToFilter);
+
+            return Ok(isFree);
+        }
+    }
+}
