fcc46b9242a72185db1b05f62318f86dc90128c8
Nana Appiah <appiah@flexberry.net>
2024-08-09 15:34:44 +0500

    Implement Test Booking sample

---
+++ IntegrationTests/Fixtures/BookingFixtures.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HAPI.Fixtures
+{
+    public class BookingFixtures
+    {
+        public static List<Booking> GetBookings() => new()
+        {
+            new Booking 
+            {
+                __PrimaryKey = Guid.NewGuid(),
+                GuestName = "Moses Arthur",
+                CheckIn = new DateTime(2024, 08, 20),
+                CheckOut =  new DateTime(2024, 08, 30),
+                Room = new Room 
+               {
+                   RoomNumber = 100,
+                   RoomType = ComfortLevelType.Luxury,
+                   Price = 450,
+                   __PrimaryKey = Guid.NewGuid(),
+               },
+                Client = new Client
+               {
+                   FirstName = "Moses",
+                   LastName = "Arthur",
+                   Email = "moses2@gmil.com",
+                   PhoneNumber = "1234567892",
+                   PassportNumber = "945123",
+               },
+            },
+        };
+    }
+}
